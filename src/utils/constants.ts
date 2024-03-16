3a81d69c34019fff2f1d750c57497fec75c7f09d
ania0 <ania000535@gmail.com>
2024-03-16 19:32:19 +0100

    add

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 
 import exp from 'constants';
 
-import tort1 from '../images/tort1.png';
+import tort1 from 'C:\Users\ania0\cake_shop\src\images\tort1.jpg';
 import tort2 from '../images/tort2.png';
 import tort3 from '../images/tort3.jpg';
 import tort4 from '../images/tort4.jpg';
